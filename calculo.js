218c73cf008319db36a1e81c55b71e728747d7da
luisanthony196 <luis.anthony.chavarria@gmail.com>
2022-05-17 00:42:44 -0500

    calculo de semana corregido

--- calculo.js
+++ calculo.js
@@ -1,9 +1,8 @@
 // Se obtiene la semana que se desea
 let currentdate = new Date();
-let oneJan = new Date(currentdate.getFullYear(),0,1);
+let oneJan = new Date(2022, 4, 9);
 let numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
-let result = Math.ceil(( currentdate.getDay() + 1 + numberOfDays) / 7);
-let uniWeek = result - 19;
+let result = Math.floor(numberOfDays / 7) + 1;
 // Se imprime el numero de la semana
 const semana = document.querySelector('#semana');
-semana.innerHTML = `Semana ${uniWeek}`;
+semana.innerHTML = `Semana ${result}`;
