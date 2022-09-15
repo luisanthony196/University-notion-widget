3345b22e9277bf43df496526aaa82ecf6ea09d52
luisanthony196 <luis.anthony.chavarria@gmail.com>
2022-09-14 19:09:52 -0500

    Actualizacion del dia inical

--- calculo.js
+++ calculo.js
@@ -1,6 +1,8 @@
 // Se obtiene la semana que se desea
 let currentdate = new Date();
-let oneJan = new Date(2022, 4, 9);
+let mesIni = 9 - 1; // Septiembre
+let oneJan = new Date(2022, mesIni, 12);
+// Calculo de la semana
 let numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
 let result = Math.floor(numberOfDays / 7) + 1;
 // Se imprime el numero de la semana
