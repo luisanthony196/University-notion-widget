1085e9cd5b573b372f6f9c9dd6f4c6f368867b39
luisanthony196 <luis.anthony.chavarria@gmail.com>
2025-03-30 17:11:06 -0500

    refactor(fecha): reiniciar conteo de fecha

--- calculo.js
+++ calculo.js
@@ -1,10 +1,10 @@
 // Se obtiene la semana que se desea
 let currentdate = new Date();
-let mesIni = 9 - 1; // Septiembre
-let oneJan = new Date(2022, mesIni, 12);
+let mesIni = 2; // Colocar mes comenzando en 0
+let oneJan = new Date(2025, mesIni, 24); // Fecha de inicio del semestre
 // Calculo de la semana
 let numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
 let result = Math.floor(numberOfDays / 7) + 1;
 // Se imprime el numero de la semana
-const semana = document.querySelector('#semana');
+const semana = document.querySelector("#semana");
 semana.innerHTML = `Semana ${result}`;
